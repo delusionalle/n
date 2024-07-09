30c5ddac6f5e51e886f6fcb9a605df59ab52ae9c
delusionalle <antiiiclimactical@gmail.com>
2024-07-09 22:11:23 +0500

    code: root layout, theme provider

---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+import 'globals.css';
+import React from 'react';
+
+import { cn } from '@/lib/utils';
+import { Inter } from 'next/font/google';
+import { Metadata } from 'next';
+import ThemeProvider from '@/components/theme-provider';
+
+export const metadata: Metadata = {
+    title: 'novo',
+    description: 'novo - college application organizer',
+};
+
+const fontSans = Inter({
+    subsets: ['latin'],
+    variable: '--font-sans',
+});
+
+function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <html lang={'en'}>
+            <body
+                className={cn(
+                    'min-h-screen bg-background font-sans antialiased',
+                    fontSans.variable,
+                )}
+            >
+                <ThemeProvider
+                    attribute='class'
+                    defaultTheme='system'
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    {children}
+                </ThemeProvider>
+            </body>
+        </html>
+    );
+}
+
+export default RootLayout;
